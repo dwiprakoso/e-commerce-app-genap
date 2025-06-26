56433001d3c35533bda8a44b48aca20e5ca11e8d
dwiprakoso <dwiprakoso015@gmail.com>
2025-06-26 23:33:48 +0700

    feat : view dashboard

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\Admin\DashboardController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+Route::get('/', [DashboardController::class, 'index'])->name('admin.index');
+// Route::prefix('dasboard')->group(function () {
+// });
