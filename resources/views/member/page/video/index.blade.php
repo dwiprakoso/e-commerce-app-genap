f3cf020128f1ac214feb37e6ce72eaf01320ef1e
dwiprakoso <dwiprakoso015@gmail.com>
2025-07-02 14:26:09 +0700

    fix : thumbnail youtube

--- resources/views/member/page/video/index.blade.php
+++ resources/views/member/page/video/index.blade.php
@@ -23,21 +23,30 @@
                                     @if (str_contains($video->url, 'youtube.com') || str_contains($video->url, 'youtu.be'))
                                         @php
                                             $videoId = '';
+                                            // YouTube watch URL
                                             if (str_contains($video->url, 'youtube.com/watch?v=')) {
-                                                $videoId = substr($video->url, strpos($video->url, 'v=') + 2);
-                                                $videoId = strpos($videoId, '&')
-                                                    ? substr($videoId, 0, strpos($videoId, '&'))
-                                                    : $videoId;
-                                            } elseif (str_contains($video->url, 'youtu.be/')) {
-                                                $videoId = substr($video->url, strpos($video->url, 'youtu.be/') + 9);
-                                                $videoId = strpos($videoId, '?')
-                                                    ? substr($videoId, 0, strpos($videoId, '?'))
-                                                    : $videoId;
+                                                parse_str(parse_url($video->url, PHP_URL_QUERY), $params);
+                                                $videoId = $params['v'] ?? '';
+                                            }
+                                            // YouTube short URL
+                                            elseif (str_contains($video->url, 'youtu.be/')) {
+                                                $videoId = basename(parse_url($video->url, PHP_URL_PATH));
+                                            }
+                                            // YouTube embed URL
+                                            elseif (str_contains($video->url, 'youtube.com/embed/')) {
+                                                $videoId = basename(parse_url($video->url, PHP_URL_PATH));
                                             }
                                         @endphp
+
                                         @if ($videoId)
-                                            <img src="https://img.youtube.com/vi/{{ $videoId }}/maxresdefault.jpg"
-                                                alt="{{ $video->title }}" class="w-full h-full object-cover">
+                                            <img src="https://img.youtube.com/vi/{{ $videoId }}/hqdefault.jpg"
+                                                alt="{{ $video->title }}" class="w-full h-full object-cover"
+                                                onerror="this.style.display='none'; this.nextElementSibling.style.display='flex';">
+                                            <!-- Fallback jika thumbnail gagal load -->
+                                            <div class="absolute inset-0 flex items-center justify-center"
+                                                style="display: none;">
+                                                <i class="fas fa-play-circle text-white text-6xl opacity-50"></i>
+                                            </div>
                                         @else
                                             <div class="absolute inset-0 flex items-center justify-center">
                                                 <i class="fas fa-play-circle text-white text-6xl opacity-50"></i>
@@ -51,11 +60,11 @@
                                 </div>
 
                                 <!-- Play Button Overlay -->
-                                <div class="absolute inset-0 flex items-center justify-center">
+                                {{-- <div class="absolute inset-0 flex items-center justify-center">
                                     <div class="bg-black bg-opacity-50 rounded-full p-4 hover:bg-opacity-70 transition">
                                         <i class="fas fa-play text-white text-2xl ml-1"></i>
                                     </div>
-                                </div>
+                                </div> --}}
                             </div>
 
                             <div class="p-6">
