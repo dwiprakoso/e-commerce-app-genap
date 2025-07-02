4abb7872b5761ba72fb93b76bef02ff082b8970b
dwiprakoso <dwiprakoso015@gmail.com>
2025-07-02 13:56:06 +0700

    fix : sidebar n topbar

--- resources/views/admin/components/sidebar.blade.php
+++ resources/views/admin/components/sidebar.blade.php
@@ -9,89 +9,68 @@
     <!-- Divider -->
     <hr class="sidebar-divider my-0">
 
-    <!-- Nav Item - Dashboard -->
-    <li class="nav-item active">
+    {{-- <!-- Nav Item - Dashboard -->
+    <li class="nav-item {{ Request::is('admin') ? 'active' : '' }}">
         <a class="nav-link" href="{{ route('admin.index') }}">
-            <i class="fas fa-fw fa-tachometer-alt"></i>
-            <span>Dashboard</span></a>
+            <i class="fas fa-tachometer-alt"></i>
+            <span>Dashboard</span>
+        </a>
     </li>
 
     <!-- Divider -->
-    <hr class="sidebar-divider">
+    <hr class="sidebar-divider"> --}}
 
     <!-- Heading -->
-    <div class="sidebar-heading">
+    {{-- <div class="sidebar-heading">
         Management
-    </div>
-    <!-- Nav Item - Kategori -->
-    <li class="nav-item">
-        <a class="nav-link collapsed" href="{{ route('admin.paket-wisata.index') }}" aria-expanded="true">
-            <i class="fas fa-fw fa-cog"></i>
+    </div> --}}
+
+    <!-- Nav Item - Paket Wisata -->
+    <li class="nav-item {{ Request::is('admin/paket-wisata*') ? 'active' : '' }}">
+        <a class="nav-link" href="{{ route('admin.paket-wisata.index') }}">
+            <i class="fas fa-suitcase-rolling"></i>
             <span>Paket Wisata</span>
         </a>
     </li>
-    <!-- Nav Item - Produk -->
-    <li class="nav-item">
-        <a class="nav-link collapsed" href="{{ route('admin.gallery.index') }}" aria-expanded="true">
-            <i class="fas fa-fw fa-cog"></i>
+
+    <!-- Nav Item - Gallery -->
+    <li class="nav-item {{ Request::is('admin/gallery*') ? 'active' : '' }}">
+        <a class="nav-link" href="{{ route('admin.gallery.index') }}">
+            <i class="fas fa-image"></i>
             <span>Gallery</span>
         </a>
     </li>
-    <!-- Nav Item - Konsumen -->
-    <li class="nav-item">
-        <a class="nav-link collapsed" href="{{ route('admin.video.index') }}" aria-expanded="true">
-            <i class="fas fa-fw fa-cog"></i>
+
+    <!-- Nav Item - Video -->
+    <li class="nav-item {{ Request::is('admin/video*') ? 'active' : '' }}">
+        <a class="nav-link" href="{{ route('admin.video.index') }}">
+            <i class="fas fa-video"></i>
             <span>Video</span>
         </a>
     </li>
-    <!-- Nav Item - Order -->
-    <li class="nav-item">
-        <a class="nav-link collapsed" href="{{ route('admin.berita.index') }}" aria-expanded="true">
-            <i class="fas fa-fw fa-cog"></i>
+
+    <!-- Nav Item - Berita -->
+    <li class="nav-item {{ Request::is('admin/berita*') ? 'active' : '' }}">
+        <a class="nav-link" href="{{ route('admin.berita.index') }}">
+            <i class="fas fa-newspaper"></i>
             <span>Berita</span>
         </a>
     </li>
-    <li class="nav-item">
-        <a class="nav-link collapsed" href="{{ route('admin.pemesanan.index') }}" aria-expanded="true">
-            <i class="fas fa-fw fa-cog"></i>
+
+    <!-- Nav Item - Pemesanan -->
+    <li class="nav-item {{ Request::is('admin/pemesanan*') ? 'active' : '' }}">
+        <a class="nav-link" href="{{ route('admin.pemesanan.index') }}">
+            <i class="fas fa-receipt"></i>
             <span>Pemesanan</span>
         </a>
     </li>
-    <li class="nav-item">
-        <a class="nav-link collapsed" href="{{ route('admin.member.index') }}" aria-expanded="true">
-            <i class="fas fa-fw fa-cog"></i>
+
+    <!-- Nav Item - Member -->
+    <li class="nav-item {{ Request::is('admin/member*') ? 'active' : '' }}">
+        <a class="nav-link" href="{{ route('admin.member.index') }}">
+            <i class="fas fa-users"></i>
             <span>Member</span>
         </a>
-    </li>
-
-
-    <!-- Divider -->
-    <hr class="sidebar-divider">
-
-    <!-- Heading -->
-    <div class="sidebar-heading">
-        Report
-    </div>
-
-    <!-- Nav Item - Pages Collapse Menu -->
-    <li class="nav-item">
-        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages"
-            aria-expanded="true" aria-controls="collapsePages">
-            <i class="fas fa-fw fa-folder"></i>
-            <span>Pages</span>
-        </a>
-        <div id="collapsePages" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
-            <div class="bg-white py-2 collapse-inner rounded">
-                <h6 class="collapse-header">Login Screens:</h6>
-                <a class="collapse-item" href="login.html">Login</a>
-                <a class="collapse-item" href="register.html">Register</a>
-                <a class="collapse-item" href="forgot-password.html">Forgot Password</a>
-                <div class="collapse-divider"></div>
-                <h6 class="collapse-header">Other Pages:</h6>
-                <a class="collapse-item" href="404.html">404 Page</a>
-                <a class="collapse-item" href="blank.html">Blank Page</a>
-            </div>
-        </div>
     </li>
 
 </ul>
